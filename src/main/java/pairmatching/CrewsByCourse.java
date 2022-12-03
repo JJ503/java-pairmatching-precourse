448e0c491980cfcdf51f14e52ceefcf9495c05ca
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 17:02:18 +0900

    feat(CrewsByCourse): 과정별 크루원 정보 저장
    docs: 과정별 크루원 정보 저장 및 관련 기능 완료 체크

---
+++ src/main/java/pairmatching/CrewsByCourse.java
@@ -0,0 +1,13 @@
+package pairmatching;
+
+import java.util.List;
+
+public class CrewsByCourse {
+    private Course course;
+    private List<Crew> name;
+
+    CrewsByCourse(Course course, List<Crew> name) {
+        this.course = course;
+        this.name = name;
+    }
+}
