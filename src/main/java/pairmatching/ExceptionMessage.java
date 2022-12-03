b8783259d3282546fdbe4edd574c0f0cd4a41a42
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 17:34:15 +0900

    feat(ExceptionMessage): 예외 메시지 상수 추가

--- src/main/java/pairmatching/ExceptionMessage.java
+++ src/main/java/pairmatching/ExceptionMessage.java
@@ -2,7 +2,11 @@ package pairmatching;
 
 public enum ExceptionMessage {
     NONE_INPUT("값을 입력해 주시길 바랍니다."),
-    INPUT_WRONG_FUNCTION_TYPE("기능 목록 중 하나를 선택해 주시길 바랍니다 (1, 2, 3, Q 중 선택).");
+    INPUT_WRONG_FUNCTION_TYPE("기능 목록 중 하나를 선택해 주시길 바랍니다 (1, 2, 3, Q 중 선택)."),
+    NONE_INPUT_THREE_CATEGORY("과정, 레벨, 미션을 모두 입력해주시길 바랍니다."),
+    INPUT_WRONG_COURSE("해당 과정을 찾지 못했습니다."),
+    INPUT_WRONG_LEVEL("해당 레벨을 찾지 못했습니다."),
+    INPUT_WRONG_MISSION("해당 미션을 찾지 못했습니다.");
 
     private final String exceptionMessage;
 
