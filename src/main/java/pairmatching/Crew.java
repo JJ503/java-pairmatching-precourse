c7a583d315b147f0815e41ca7abd65922138ee09
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 17:02:00 +0900

    feat(Crew): pair 이름 데이터 초기화 기능 추가
    docs: 크루원 정보 저장 및 관련 기능 완료 체크

--- src/main/java/pairmatching/Crew.java
+++ src/main/java/pairmatching/Crew.java
@@ -1,11 +1,16 @@
 package pairmatching;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Crew {
     private Course course;
     private String name;
+    private List<String> pairName;
 
     Crew (Course course, String name) {
         this.course = course;
         this.name = name;
+        pairName = new ArrayList<>();
     }
 }
