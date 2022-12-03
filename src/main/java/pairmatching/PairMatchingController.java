af0b92bfc8b9388254e53a657cf4c2f598feac82
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 16:21:43 +0900

    feat(PairMatchingController): 기능 선택 입력 값 유효성 검증 기능 추가
    docs: 기능 선택 입력 받기 및 관련 기능 완료 체크

--- src/main/java/pairmatching/PairMatchingController.java
+++ src/main/java/pairmatching/PairMatchingController.java
@@ -1,5 +1,7 @@
 package pairmatching;
 
+import java.util.Arrays;
+
 public class PairMatchingController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
@@ -12,5 +14,22 @@ public class PairMatchingController {
         outputView.printMenuList();
         String inputValue = inputView.readOption();
         System.out.println(inputValue);
+    }
+
+    private void validateOption(String option) {
+        if (isEmpty(option)) {
+            ExceptionMessage.NONE_INPUT.throwException();
+        }
+        if (isExistOption(option)) {
+            ExceptionMessage.INPUT_WRONG_FUNCTION_TYPE.throwException();
+        }
+    }
+
+    private boolean isEmpty(String validateInput) {
+        return validateInput.isEmpty();
+    }
+
+    private boolean isExistOption(String option) {
+        return Arrays.stream(Menu.values()).anyMatch(menu -> menu.isMatchOption(option));
     }
 }
