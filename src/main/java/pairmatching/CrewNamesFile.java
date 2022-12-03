9b9095c87383ec8a1a543132ddbdaf5709e93d35
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 16:27:46 +0900

    feat(CrewNamesFile): 크루 이름 목록 파일 관련 상수 선언 추가

---
+++ src/main/java/pairmatching/CrewNamesFile.java
@@ -0,0 +1,14 @@
+package pairmatching;
+
+public enum CrewNamesFile {
+    BACKEND_CREW(Course.BACKEND, "src/main/resources/backend-crew.md"),
+    FRONTEND_CREW(Course.FRONTEND, "src/main/resources/frontend-crew.md");
+
+    private final Course course;
+    private final String filePath;
+
+    CrewNamesFile(Course course, String filePath) {
+        this.course = course;
+        this.filePath = filePath;
+    }
+}
