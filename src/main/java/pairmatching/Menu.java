5cd44d94be593d2bfae123a901e741fc88faeb6c
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 15:37:48 +0900

    feat(Menu): Menu 관련 상수 선언

---
+++ src/main/java/pairmatching/Menu.java
@@ -0,0 +1,16 @@
+package pairmatching;
+
+public enum Menu {
+    PAIR_MATH("1", "페어 매칭"),
+    PAIR_CHECK("2", "페어 조회"),
+    PAIR_INITIALIZATION("3", "페어 초기화"),
+    END_PROGRAM("Q", "종료");
+
+    private final String option;
+    private final String functionType;
+
+    Menu(String option, String functionType) {
+        this.option = option;
+        this.functionType = functionType;
+    }
+}
