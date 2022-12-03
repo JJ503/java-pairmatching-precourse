1430882cc9e47c02904f58207c73dfc152df9391
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 16:35:42 +0900

    feat(Crew): 크루원 정보 저장 기능 추가
    docs: 크루원 정보 저장 및 관련 기능 완료 체크

---
+++ src/main/java/pairmatching/Crew.java
@@ -0,0 +1,11 @@
+package pairmatching;
+
+public class Crew {
+    private Course course;
+    private String name;
+
+    Crew (Course course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+}
