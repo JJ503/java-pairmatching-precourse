ee4515a4f9e12f6c02a72e89e1f9c60cb15dae79
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 17:03:08 +0900

    feat(PairMatchingController): 크루 이름 파일 입력 받기 기능 추가
    docs: 크루 이름 파일 입력 받기 및 관련 기능 완료 체크

--- src/main/java/pairmatching/PairMatchingController.java
+++ src/main/java/pairmatching/PairMatchingController.java
@@ -1,19 +1,55 @@
 package pairmatching;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PairMatchingController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
 
     public void startProgram() {
+        List<CrewsByCourse> crewsByCourses = initCrewList();
         selectOption();
+    }
+
+    private List<CrewsByCourse> initCrewList() {
+        List<CrewsByCourse> crewsByCourses = new ArrayList<>();
+        for (CrewNamesFile crewFile : CrewNamesFile.values()) {
+            List<String> crewNames = getNameList(crewFile.getFilePath());
+            List<Crew> crews = setCrewList(crewNames, crewFile.getCourse());
+            crewsByCourses.add(new CrewsByCourse(crewFile.getCourse(), crews));
+        }
+
+        return crewsByCourses;
+    }
+
+    private List<String> getNameList(String path) {
+        EnterCrewFile enterCrewFile = new EnterCrewFile();
+        try {
+            return enterCrewFile.enterFileToList(path);
+        } catch (IOException exception) {
+            System.out.println(exception);
+        }
+
+        return null;
+    }
+
+    private List<Crew> setCrewList(List<String> crewNames, Course course) {
+        List<Crew> crews = new ArrayList<>();
+
+        for (String name : crewNames) {
+            crews.add(new Crew(course, name));
+        }
+
+        return crews;
     }
 
     private void selectOption() {
         outputView.printMenuList();
         String inputValue = inputView.readOption();
-        System.out.println(inputValue);
+        validateOption(inputValue);
     }
 
     private void validateOption(String option) {
