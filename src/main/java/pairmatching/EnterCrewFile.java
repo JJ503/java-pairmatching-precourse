cd9a0d476658e8f8610dcab6ca82e413091c63d7
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-03 15:04:04 +0900

    feat(EnterCrewFile): 파일 내용을 List<String> 형으로 반환 기능 추가
    docs: 파일 읽기 완료 체크

--- src/main/java/pairmatching/EnterCrewFile.java
+++ src/main/java/pairmatching/EnterCrewFile.java
@@ -1,15 +1,29 @@
 package pairmatching;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnterCrewFile {
-    public void enterFileToList(String path) throws IOException {
-        readFile(path);
+    public List<String> enterFileToList(String path) throws IOException {
+        return fileToList(readFile(path));
     }
 
     private BufferedReader readFile(String path) throws FileNotFoundException {
         File file = new File(path);
         FileReader filereader = new FileReader(file);
         return new BufferedReader(filereader);
+    }
+
+    private List<String> fileToList(BufferedReader bufferedReader) throws IOException {
+        List<String> crewNames = new ArrayList<>();
+        String name;
+        
+        while((name = bufferedReader.readLine()) != null){
+            crewNames.add(name);
+        }
+        bufferedReader.close();
+
+        return crewNames;
     }
 }
