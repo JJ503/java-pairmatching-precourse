03dd068dc897ff5540c966d16ce94b99085ea619
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-05 23:42:44 +0900

    feat(InputView): 재매칭 여부 선택 입력 기능 추가
    docs: 재매칭 여부 선택 입력 완료 체크

--- src/main/java/pairmatching/InputView.java
+++ src/main/java/pairmatching/InputView.java
@@ -3,11 +3,18 @@ package pairmatching;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String ASK_REMATCH = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n네 | 아니오";
+
     public String readOption() {
         return Console.readLine();
     }
 
     public String readCategory() {
+        return Console.readLine();
+    }
+
+    public String readAskRematchPair() {
+        System.out.println(ASK_REMATCH);
         return Console.readLine();
     }
 }
