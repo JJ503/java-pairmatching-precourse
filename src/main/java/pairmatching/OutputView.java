fc6ea367090d9c71e4a50cdc40a63f1d8c408c53
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 11:23:00 +0900

    feat(OutputView): 페어 매칭 결과 출력 기능 추가
    docs: 페어 매칭 결과 출력 완료 체크

--- src/main/java/pairmatching/OutputView.java
+++ src/main/java/pairmatching/OutputView.java
@@ -9,6 +9,9 @@ public class OutputView {
     private static final String EXAMPLE_PAIR_SEARCH = "과정, 레벨, 미션을 선택하세요.\nex) 백엔드, 레벨1, 자동차경주";
     private static final String COURSE = "과정: %s\n";
     private static final String MISSION = "미션:";
+    private static final String COURSE_DELIMITER = " | ";
+    private static final String PAIR_MATCH_RESULT_MESSAGE = "페어 매칭 결과입니다.";
+    private static final String PAIR_RESULT_DELIMITER = " : ";
 
     public void printMenuList() {
         System.out.println(MENU_LIST_MESSAGE);
@@ -26,7 +29,7 @@ public class OutputView {
     }
 
     private void printCourse() {
-        System.out.printf(COURSE, String.join(" | ", getCourses()));
+        System.out.printf(COURSE, String.join(COURSE_DELIMITER, getCourses()));
     }
 
     private List<String> getCourses() {
@@ -42,6 +45,14 @@ public class OutputView {
         System.out.println(MISSION);
         for (Level level : Level.values()) {
             System.out.println(level.getMissionMessage());
+        }
+    }
+
+    public void printPairMatchResult(List<List<String>> pairs) {
+        System.out.println(PAIR_MATCH_RESULT_MESSAGE);
+
+        for (List<String> pair : pairs) {
+            System.out.println(String.join(PAIR_RESULT_DELIMITER, pair));
         }
     }
 }
